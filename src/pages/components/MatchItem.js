f84c0175dce0d44ebb91a5ad784991c4a732ab4a
Amaury Garcia <amaugarci@outlook.com>
2023-04-20 22:57:23 +0200

    Fixing property of undefined in MatchItem

--- src/pages/components/MatchItem.js
+++ src/pages/components/MatchItem.js
@@ -7,8 +7,12 @@ export default function (props) {
     const { match, team1, team2 } = props
     return (
         <Card sx={{ mt: 1, p: 2 }}>
-            <TeamItem team={team1} win={match?.result == 1 ? 2 : match?.result == 0 ? 1 : match?.result == 2 ? 0 : undefined} />
-            <TeamItem team={team2} sx={{ mt: 1 }} win={match?.result == 2 ? 2 : match?.result == 0 ? 1 : match?.result == 1 ? 0 : undefined} />
+            {match &&
+                <>
+                    <TeamItem team={team1} win={match?.result == 1 ? 2 : match?.result == 0 ? 1 : match?.result == 2 ? 0 : undefined} />
+                    <TeamItem team={team2} sx={{ mt: 1 }} win={match?.result == 2 ? 2 : match?.result == 0 ? 1 : match?.result == 1 ? 0 : undefined} />
+                </>
+            }
         </Card>
     )
 }

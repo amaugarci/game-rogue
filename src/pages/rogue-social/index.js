1c039bd3e8faa79d13a03c6c660df95187460baf
Amaury Garcia <amaugarci@outlook.com>
2023-05-24 03:00:04 +0200

    Fixing client side error

--- src/pages/rogue-social/index.js
+++ src/pages/rogue-social/index.js
@@ -112,39 +112,39 @@ const Page = (props) => {
 };
 
 Page.getLayout = (page) => {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 10000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 10000);
+  // }, []);
 
-  if (loading === true) {
-    return (
-      <Box
-        sx={{
-          backgroundColor: "black",
-          position: "fixed",
-          width: "100%",
-          height: "100%",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          zIndex: 9999,
-        }}
-        className="rogue-social-splash"
-      >
-        {/* <InfinitySpin
-          width='200'
-          color={theme.palette.primary.main}
-        /> */}
-        <Box sx={{ position: "relative", zIndex: 9999 }}>
-          <img src="/rogue_social.gif" />
-        </Box>
-      </Box>
-    );
-  }
+  // if (loading === true) {
+  //   return (
+  //     <Box
+  //       sx={{
+  //         backgroundColor: "black",
+  //         position: "fixed",
+  //         width: "100%",
+  //         height: "100%",
+  //         top: "50%",
+  //         left: "50%",
+  //         transform: "translate(-50%, -50%)",
+  //         zIndex: 9999,
+  //       }}
+  //       className="rogue-social-splash"
+  //     >
+  //       {/* <InfinitySpin
+  //         width='200'
+  //         color={theme.palette.primary.main}
+  //       /> */}
+  //       <Box sx={{ position: "relative", zIndex: 9999 }}>
+  //         <img src="/rogue_social.gif" />
+  //       </Box>
+  //     </Box>
+  //   );
+  // }
 
   return (
     <TournamentProvider>

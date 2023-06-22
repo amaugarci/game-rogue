387d0d5f25793564a4ee59c12779d2685b6bb6d6
Amaury Garcia <amaugarci@outlook.com>
2023-06-22 21:27:18 +0200

    Fixed empty organization list

--- src/pages/rogue-social/organization/index.js
+++ src/pages/rogue-social/organization/index.js
@@ -1,12 +1,25 @@
+import TournamentProvider, { useTournamentContext } from "@/src/context/TournamentContext";
+
 import { Box } from "@mui/material";
 import Organizations from "@/src/components/widgets/rogue-social/accounts/Organizations";
 import PublicLayout from "@/src/content/PublicLayout";
-import TournamentProvider from "@/src/context/TournamentContext";
+import { useAuthContext } from "@/src/context/AuthContext";
+import { useMemo } from "react";
 
 const Page = (props) => {
+  const { user } = useAuthContext();
+  const { organization } = useTournamentContext();
+
+  const myOrganizations = useMemo(() => {
+    if (organization?.organizations) {
+      return _.filter(organization.organizations, (val) => val.uid === user.id);
+    }
+    return [];
+  }, [organization?.organizations, user]);
+
   return (
     <Box>
-      <Organizations />
+      <Organizations items={myOrganizations} />
     </Box>
   );
 };

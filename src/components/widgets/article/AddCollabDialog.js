8ecdcc0edda427f3571f3a4f6ca08df1581e40b9
Amaury Garcia <amaugarci@outlook.com>
2023-07-12 00:17:55 +0200

    Fixed issue with adding collaborator

--- src/components/widgets/article/AddCollabDialog.js
+++ src/components/widgets/article/AddCollabDialog.js
@@ -4,6 +4,7 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  FormHelperText,
   TextField
 } from "@mui/material";
 
@@ -11,13 +12,21 @@ import { useState } from "react";
 import { useTournamentContext } from "@/src/context/TournamentContext";
 
 export default function AddCollabDialog({ aid, open, onClose }) {
-  const { article } = useTournamentContext();
+  const { article, player } = useTournamentContext();
   const [value, setValue] = useState("");
+  const [error, setError] = useState(null);
 
   const onSave = () => {
-    article.update(aid, {
-      collabs: [...article.articles[aid].collabs, value]
-    });
+    const id = _.findKey(player.players, (val) => val._id === value);
+    if (id) {
+      setError(null);
+      article.update(aid, {
+        collabs: [...article.articles[aid].collabs, id]
+      });
+      onClose();
+    } else {
+      setError("Invalid Rogue ID");
+    }
   };
 
   return (
@@ -25,12 +34,14 @@ export default function AddCollabDialog({ aid, open, onClose }) {
       <DialogTitle>Add a collaborator</DialogTitle>
       <DialogContent sx={{ px: 2 }}>
         <TextField
+          error={error !== null}
           variant="outlined"
           placeholder="Enter Rogue ID"
           value={value}
           onChange={(e) => setValue(e.target.value)}
           fullWidth
         />
+        {error && <FormHelperText error>{error}</FormHelperText>}
       </DialogContent>
       <DialogActions sx={{ pb: 2, px: 2 }}>
         <Button variant="contained" onClick={onSave}>

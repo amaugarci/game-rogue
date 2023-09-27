e9d10c894d176620dc2892c7f10743d5bb2cdf25
Amaury Garcia <amaugarci@outlook.com>
2023-09-27 04:48:00 +0200

    Fixed module not found issue in organizer create page

--- src/pages/organizer/create/index.js
+++ src/pages/organizer/create/index.js
@@ -1,7 +1,7 @@
 import { Paper, useTheme } from "@mui/material";
 
 import AdminLayout from "@/src/content/AdminLayout";
-import OrganizationCreateForm from "@/src/components/widgets/organizer/OrganizationCreateForm";
+import OrganizationCreateForm from "@/src/components/widgets/organizer/OrganizerCreateForm";
 import { useAppContext } from "@/src/context/app";
 // import ColorPicker from "@mapbox/react-colorpickr";
 import { useEffect } from "react";

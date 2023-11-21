de8b2c93865e638559609cfcc2a3bfdc031873a6
Amaury Garcia <amaugarci@outlook.com>
2023-11-21 15:32:07 +0900

    Update plus-plans.js

--- src/pages/plus-plans.js
+++ src/pages/plus-plans.js
@@ -1,9 +1,8 @@
 import PublicLayout from "@/src/content/PublicLayout";
 import TournamentProvider from "@/src/context/TournamentContext";
-import PlusPlan from '@/src/pages/plus plan main page/plus-plan';
 
 const Page = (props) => {
-  return <PlusPlan />;
+  return <></>;
 };
 
 Page.getLayout = (page) => {
